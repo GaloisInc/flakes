4e6f60e06278fa1f78e235230eb8f72c614e4def
Kevin Quick <kquick@galois.com>
2024-01-09 20:32:23 -0800

    Use newer nixpkgs where possible and add cvc5 versions through 1.1.0

--- flake.nix
+++ flake.nix
@@ -12,7 +12,35 @@
   #   $ nix run github:GaloisInc/flakes#z3.v4_8_14 -- -version
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/23.05";
+
+    nixpkgs_2205.url = "github:nixos/nixpkgs/22.05";
+    # nixpkgs circa 22.05 is sufficient to build z3, yices, boolector (check
+    # disabled), cvc4, cvc5, and stp, but not bitwizla_0_3_0.
+
+    # nixpkgs circa 23.05 or later is needed for bitwuzla_0_3_0 (specifically for
+    # meson >= 0.64), but 23.05 or later fails for:
+    #
+    #    * yices-2.5.4, (multiple definition of smt_opcodes_)t
+    #      during linking)
+    #
+    #    * cvc5-1.0.0, cvc5-1.0.1, cvc5-1.0.2, nixpkgs 23.05
+    #
+    #      (src/parser/smt2/Smt2.g:
+    #         Decision can match input such as "LPAREN_TOK INDEX_TOK" using multiple alternatives: 1, 2
+    #         As a result, alteriative(s) 2 were disabled for that input
+    #       src/expr/node_manager_template.h:0
+    #         undefined replacement ${metakind_mkConstDelete}
+    #      )
+    #
+    #    * cvc5-1.0.0 through cvc5-1.0.6, nixpkgs 23.11
+    #
+    #      configuration fails because these older cvc5 versions use python with
+    #      the "toml" package, which is deprecated in favor of the "tomli"
+    #      package and is therefore not part of the later nixpkgs version.
+    #
+    # using: gcc 12.2, openjdk-19.0.2
+    nixpkgs.url = "github:nixos/nixpkgs/23.11";
+
     flake-utils.url = "github:numtide/flake-utils";
     abc_src_2020_06_22 = {
       url = "github:berkeley-abc/abc/341db2566";
@@ -60,6 +88,30 @@
     };
     cvc5_src_1_0_2 = {
       url = "github:cvc5/cvc5/cvc5-1.0.2";
+      flake = false;
+    };
+    cvc5_src_1_0_3 = {
+      url = "github:cvc5/cvc5/cvc5-1.0.3";
+      flake = false;
+    };
+    cvc5_src_1_0_6 = {
+      url = "github:cvc5/cvc5/cvc5-1.0.6";
+      flake = false;
+    };
+    cvc5_src_1_0_7 = {
+      url = "github:cvc5/cvc5/cvc5-1.0.7";
+      flake = false;
+    };
+    cvc5_src_1_0_8 = {
+      url = "github:cvc5/cvc5/cvc5-1.0.8";
+      flake = false;
+    };
+    cvc5_src_1_0_9 = {
+      url = "github:cvc5/cvc5/cvc5-1.0.9";
+      flake = false;
+    };
+    cvc5_src_1_1_0 = {
+      url = "github:cvc5/cvc5/cvc5-1.1.0";
       flake = false;
     };
     stp_src_2_3_3 = {
@@ -125,9 +177,24 @@
   outputs = inps@{ self, ... }:
     inps.flake-utils.lib.eachDefaultSystem (system:
       let pkgs = inps.nixpkgs.legacyPackages.${system};
+          pkgs22 = inps.nixpkgs_2205.legacyPackages.${system};
           cleanVer = builtins.replaceStrings ["."] ["_"];
+          # The mkVerPkg will build the package as specified by the *current*
+          # pkgs specification (imported from nixpkgs), but adjusting the src and
+          # version to the specified target.  This (lazy methodology) assumes
+          # that the build process is valid for each target src/version
+          # specified; where this is not the case, a more specific directive is
+          # given (e.g. yices-2.5.4).  A possible future, more disciplined
+          # approach would be to maintain local build instructions that can be
+          # adjusted for each version, providing better separation from nixpkgs
+          # dependencies, but losing the benefit of common development efforts.
           mkVerPkg = pkg: version:
             pkgs.${pkg}.overrideAttrs  (_: {
+              inherit version;
+              src = inps."${pkg + "_src_" + cleanVer version}";
+            });
+          mkVerPkg22 = pkg: version:
+            pkgs22.${pkg}.overrideAttrs  (_: {
               inherit version;
               src = inps."${pkg + "_src_" + cleanVer version}";
             });
@@ -144,10 +211,12 @@
               buildInputs = old.buildInputs ++ [ pkgs.symfpu ];
               # Adding --symfpu to configureFlags is ineffective ?!
               # configureFlags = old.configureFlags ++ [ "--symfpu" ];
-              cmakeFlags = old.cmakeFlags ++ [ "-DUSE_SYMFPU=ON" ];
+              cmakeFlags = (old.cmakeFlags or []) ++ [ "-DUSE_SYMFPU=ON" ];
             });
           mkCVC5 = mkVerPkg "cvc5";
+          mk22CVC5 = mkVerPkg22 "cvc5";
           mkYices = mkVerPkg "yices";
+          mk22Yices = mkVerPkg22 "yices";
           mkZ3 = mkVerPkg "z3";
           mkSTP = mkVerPkg "stp";
       in
@@ -162,20 +231,20 @@
             v4_8_13 = mkZ3 "4.8.13";
             v4_8_14 = mkZ3 "4.8.14";
           };
-          yices = pkgs.yices // { # whatever the nixpkgs current version is...
-            v2_6   = mkYices "2.6.4";
+          yices = pkgs.yices // rec { # whatever the nixpkgs current version is...
+            v2_6   = v2_6_4;
+            v2_5   = v2_5_4;
             v2_6_4 = mkYices "2.6.4";
             v2_6_2 = mkYices "2.6.2";
-            v2_5_4 = mkYices "2.5.4";
-            v2_5   = mkYices "2.5.4";
+            v2_5_4 = mk22Yices "2.5.4";
           };
-          stp = pkgs.stp // { # whatever the nixpkgs current version is...
+          stp = pkgs.stp // rec { # whatever the nixpkgs current version is...
             v2_3   = mkSTP "2.3.3";
-            v2_2   = mkSTP "2.2.0";
+            v2_2   = v2_2_0;
             v2_3_3   = mkSTP "2.3.3";
             v2_3_2   = mkSTP "2.3.2";
             v2_3_1   = mkSTP "2.3.1";
-            v2_2_0   = mkSTP "2.2.0";
+            v2_2_0   = mkVerPkg22 "stp" "2.2.0";
           };
           cvc4 = pkgs.cvc4 // { # whatever the nixpkgs current version is...
             v1_8   = mkCVC4 "1.8";
@@ -183,10 +252,21 @@
             v4_1_8   = mkCVC4 "1.8";
             v4_1_7   = mkCVC4 "1.7";
           };
-          cvc5 = pkgs.cvc5 // { # whatever the nixpkgs current version is...
-            v1_0_0  = mkCVC5 "1.0.0";
-            v1_0_1  = mkCVC5 "1.0.1";
-            v1_0_2  = mkCVC5 "1.0.2";
+          cvc5 = pkgs.cvc5 // rec { # whatever the nixpkgs current version is...
+            v1_0  = v1_0_9;
+            v1_1  = v1_1_0;
+            v1_0_0  = mk22CVC5 "1.0.0";
+            v1_0_1  = mk22CVC5 "1.0.1";
+            v1_0_2  = mk22CVC5 "1.0.2";
+            v1_0_3  = mk22CVC5 "1.0.3";
+            # n.b. v 1.0.4 and 1.0.5 have dependencies on the obsolete python
+            # "toml" package, which isn't available under nixpkgs.  These are
+            # older anyhow, so these are skipped.
+            v1_0_6  = mkCVC5 "1.0.6";
+            v1_0_7  = mkCVC5 "1.0.7";
+            v1_0_8  = mkCVC5 "1.0.8";
+            v1_0_9  = mkCVC5 "1.0.9";
+            v1_1_0  = mkCVC5 "1.1.0";
           };
           boolector = pkgs.boolector // { # whatever the nixpkgs current version is...
             v3_2   = mkBoolector "3.2.2";
