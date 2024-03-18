207fc052a9bb77160caa942a13dba80c638de746
Kevin Quick <kquick@galois.com>
2024-03-18 12:54:06 -0700

    Updates to the build-bom wrapper including WAF support.

--- build-bom/wrapper.nix
+++ build-bom/wrapper.nix
@@ -17,25 +17,69 @@
 , gnumake, gnutar, bintools
 }:
 
-{ clang ? pkgs.clang_14
-, llvm ? pkgs.llvm_14
+{ clang ? pkgs.clang_16
+, llvm ? pkgs.llvm_16
 , build-bom ? default-build-bom
-, extra-build-bom-flags ? ""
+, extra-build-bom-flags ? []
 , extra-buildInputs ? []
 }:
 
-drv: drv.overrideAttrs(oldAttrs:
-  let
-    bld =
+drv:
+let
+  defaultBuilder = "${pkgs.bash}/bin/bash";
+  updDrv =
+    let drv1 = drv.overrideAttrs (oldAttrs:
+          (bld oldAttrs) //
+          {
+            nativeBuildInputs = bldInputs oldAttrs;
+            NIX_CC_USE_RESPONSE_FILE=0;  # do not use a response file for args
+            NIX_HARDENING_ENABLE="";  # disable hardening
+          });
+    in drv1;
+  bld = oldAttrs:
+    if builtins.elem pkgs.wafHook (oldAttrs.buildInputs or []) ||
+       builtins.elem pkgs.wafHook (oldAttrs.propagatedBuildInputs or [])
+    then
+      # Adding the wafHook causes waf build/install operations to be inserted by
+      # setup_hook at execution time, not at the derivation specification time
+      # (does this violate process hash generation?), so this must be manually
+      # overridden here.  Copied/modified from
+      # nixpkgs/pkgs/development/tools/build-managers/waf/setup-hook.sh with
+      # escaping and prefixing the waf invocation with build-bom.
+      {
+        buildPhase = ''
+          runHook preBuild
+
+          # set to empty if unset
+          : "''${wafFlags=}"
+
+          local flagsArray=(
+            ''${enableParallelBuilding:+-j ''${NIX_BUILD_CORES}}
+            $wafFlags ''${wafFlagsArray[@]}
+            $wafBuildFlags ''${wafBuildFlagsArray[@]}
+            ''${wafBuildTargets:-build}
+          )
+
+          echoCmd 'waf build flags' "''${flagsArray[@]}"
+          ${build-bom}/bin/build-bom generate-bitcode ${builtins.concatStringsSep " " extra-build-bom-flags} -- python "$wafPath" "''${flagsArray[@]}"
+
+          runHook postBuild
+        '';
+      }
+    else
       if oldAttrs ? buildPhase
       then {
         buildPhase = ''
-          ${build-bom}/bin/build-bom generate-bitcode -- /bin/bash -c "${oldAttrs.buildPhase}"
+          echo "building with build-bom and existing build phase"
+          ${build-bom}/bin/build-bom generate-bitcode ${builtins.concatStringsSep " " extra-build-bom-flags} -- ${pkgs.bash}/bin/bash -c "${oldAttrs.buildPhase}"
         '';
       }
       else {
         # copied from nixpkgs/pkgs/stdenv/generic/setup.sh, with escaping and
-        # replacing the invocation of make with make via build-bom.
+        # replacing the invocation of make with make via build-bom.  Extended to
+        # *directly* support waf (the waf hook re-configures during setupPhase,
+        # rather than directly updating the derivation, so this side-steps that
+        # by looking for the waf file and performing the waf build).
         buildPhase = ''
           runHook preBuild
 
@@ -72,22 +116,29 @@ drv: drv.overrideAttrs(oldAttrs:
               _accumFlagsArray makeFlags makeFlagsArray buildFlags buildFlagsArray
 
               echoCmd 'build flags' "''${flagsArray[@]}"
-              ${build-bom}/bin/build-bom generate-bitcode ${extra-build-bom-flags} -- ${gnumake}/bin/make ''${makefile:+-f $makefile} "''${flagsArray[@]}"
+              ${build-bom}/bin/build-bom generate-bitcode ${builtins.concatStringsSep " " extra-build-bom-flags} -- ${gnumake}/bin/make ''${makefile:+-f $makefile} "''${flagsArray[@]}"
               unset flagsArray
           fi
 
           runHook postBuild
         '';
       };
-  in bld // rec {
-    outputs = (oldAttrs.outputs or [ "out" ]) ++ [ "bc" ];
-    nativeBuildInputs = (oldAttrs.nativeBuildInputs or []) ++ [
-      clang
-      llvm
-      bintools
-      gnutar
-    ] ++ extra-buildInputs;
-    postInstall = ''
+  bldInputs = oldAttrs:
+    let moreInputs = [
+          clang
+          llvm
+          bintools
+          gnutar
+        ];
+        fullInputs = (oldAttrs.nativeBuildInputs or []) ++
+                     moreInputs ++
+                     extra-buildInputs;
+    in fullInputs;
+  gen_bc_out = forTgts: ''
+      echo generate $bc from $out for ${forTgts}
+      mkdir $out
+      cp -r ${forTgts}/* $out/
+
       mkdir $bc
       extract_bc() {
         (
@@ -95,7 +146,10 @@ drv: drv.overrideAttrs(oldAttrs:
           for X in $(${pkgs.findutils}/bin/find . -type f); do
             if ${pkgs.file}/bin/file $X | grep -q ELF ; then
               mkdir -p $bc/$(dirname $X)
-              ${build-bom}/bin/build-bom extract-bitcode -o $bc/$X.bc $X
+              ${build-bom}/bin/build-bom extract-bitcode -v \
+                  --llvm-link ${llvm}/bin/llvm-link \
+                  --objcopy ${bintools}/bin/objcopy \
+                  $X -o $bc/$X.bc
             fi
           done
         )
@@ -103,7 +157,14 @@ drv: drv.overrideAttrs(oldAttrs:
 
       if [ "X$out" != "X" ] ; then extract_bc $out; fi
       if [ "X$bin" != "X" ] ; then extract_bc $bin; fi
+  '';
 
-      ${oldAttrs.postInstall or "# nada"}
-      '';
-})
+  wrapDrv = pkgs.stdenv.mkDerivation {
+    name = drv.name + "-bc";
+    src = updDrv.src;
+    buildPhase = "";
+    installPhase = gen_bc_out updDrv.out;
+    outputs = [ "out" "bc" ];
+  };
+
+in wrapDrv
