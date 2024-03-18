da3ebbd1154a9b7a49d56aec9dc240edfefa5d82
Kevin Quick <kquick@galois.com>
2024-03-18 14:41:33 -0700

    Cleanup of build-bom default.nix

--- build-bom/default.nix
+++ build-bom/default.nix
@@ -1,21 +1,20 @@
 { pkgs
-# , clang ? pkgs.clang_12
-# , llvm ? pkgs.llvm_12
+, clang ? pkgs.clang_12
+, llvm ? pkgs.llvm_12
 , src ? pkgs.fetchFromGitHub
   { owner = "travitch"; repo = "build-bom";
     rev = "14602f7"; # "master";
   }
-, postUnpack ? null
+, postUnpack ? ""
 }:
 
-builtins.trace ("generating build-bom " + (if postUnpack == null then "regular" else "with postUnpack"))
 pkgs.rustPlatform.buildRustPackage {
   pname = "build-bom";
   version = "0.1.0";
   src = src;
   doCheck = false;
-  nativeBuildInputs = [ pkgs.clang_12 pkgs.llvm_12 ];
+  nativeBuildInputs = [ clang llvm ];
   # cargoSha256 = lib.fakeSha256;
   cargoSha256 = "sha256-k2NGJWj8IKMeNUTbdSFquaqcDCN+lBNzpfc7HAXufrA=";
-  inherit postUnpack;
+  postUnpack = if postUnpack == null then "" else postUnpack;
 }
